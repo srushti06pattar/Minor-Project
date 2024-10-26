1e1ddd5db05a099c2c8110423d903e9df5f36c2c
srushti06pattar <srushtipattar0623@gmail.com>
2024-10-26 22:24:27 +0530

    change

--- LoginPage/clients/src/App.jsx
+++ LoginPage/clients/src/App.jsx
@@ -1,8 +1,13 @@
 import './App.scss'
+import Dashoboard from './Components/Dashboard/Dashoboard'
+import Login from './Components/Login/Login'
+import Register from './Components/Register/Register'
 function App() {
   return (
     <div>
-    App
+    <Dashoboard/>
+    <Login/>
+    <Register/>
     </div>
   )
 }
