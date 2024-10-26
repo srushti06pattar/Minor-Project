bdc864d39c970a9bd72fba628d28b1cdf9b2b40c
srushti06pattar <srushtipattar0623@gmail.com>
2024-10-26 22:39:22 +0530

    change

--- LoginPage/clients/src/App.jsx
+++ LoginPage/clients/src/App.jsx
@@ -2,6 +2,28 @@ import './App.scss'
 import Dashoboard from './Components/Dashboard/Dashoboard'
 import Login from './Components/Login/Login'
 import Register from './Components/Register/Register'
+
+//Import router react dom
+
+import{
+  createBrowserRouter,
+  RouterProvider
+}from 'react-router-dom'
+
+const router =createBrowserRouter([
+  {
+    path: '/',
+    element : <div><Login/></div>
+  },
+  {
+    path: '/',
+    element : <div><Dashoboard/></div>
+  },
+  {
+    path: '/',
+    element : <div><Register/></div>
+  }
+])
 function App() {
   return (
     <div>
