55b1f3708409ab6223494d0f532ca75a77b0eb9d
srushti06pattar <srushtipattar0623@gmail.com>
2024-10-27 18:11:06 +0530

    change

--- LoginPage/clients/src/Components/Login/Login.jsx
+++ LoginPage/clients/src/Components/Login/Login.jsx
@@ -1,16 +1,29 @@
 import React from 'react'
 import './Login.css'
+import {Link} from 'react-router-dom'
+import video from '../../LoginAssets/video.mp4'
 
 const Login = () => {
   return (
-    <div>
-      <a href="/register"> TO Register</a>
-      <br/>
-      <a href="/dashboard"> TO Dashboard</a>
-      <br/>
-      this is login page
+    <div className="loginPage flex">
+      <div className="container flex">
+        <div className="videoDiv">
+          <video src={video} autoPlay muted loop></video>
+
+          <div className="textDiv">
+            <h2 className="title">Create and shell Extradoniary Products</h2>
+            <p>Adopt the peace of nature!</p>
+          </div>
+          
+          <div className="footerDiv flex">
+            <span className="text">Don't Have an Account?</span>
+            <Link to={'/register'} >
+            <button className='btn'>Sign Up</button>
+            </Link>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
-
 export default Login
--- LoginPage/clients/src/Components/Register/Register.jsx
+++ LoginPage/clients/src/Components/Register/Register.jsx
@@ -5,7 +5,7 @@ const Register = () => {
   return (
     <div>
       this is register page
-      <a href="/Login">TO Login</a>
+      <a href="/login">TO Login</a>
     </div>
   )
 }
